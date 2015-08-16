9bff0997ab4e7f3e3152d7dc6f870712a572f84c
zwelchcb <Zachary.Welch@careerbuilder.com>
2015-08-16 14:57:59 -0400

    edit friend

--- app/controllers/friends/edit.js
+++ app/controllers/friends/edit.js
@@ -4,8 +4,8 @@ import FriendsBaseController from './base';
 export default FriendsBaseController.extend({
   actions: {
     cancel: function() {
-      this.transitionToRoute('friends');
+      this.transitionToRoute('friends.show', this.get('model'));
       return false;
     }
-  }
+  }  
 });
